34c7b4ac03b881fa01e6af534908860994e24276
Bernard Fyneface <bernardfyneface8@gmail.com>
2023-06-20 16:38:39 -0700

    Print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * Write a program the alphabet 10 times
+ * followed by a new line
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	int b;
+
+	for (b = 1; a <= 10; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}
