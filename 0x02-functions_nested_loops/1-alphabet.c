7c841e42c662a113f25f44306ebd7bace9865ecb
Bernard Fyneface <bernardfyneface8@gmail.com>
2023-06-20 16:10:08 -0700

    Print Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Write a function that prints 10 times the alphabet,
+ * in lowercase followed by a new line, and returns 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 127; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
