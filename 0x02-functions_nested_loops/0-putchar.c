2b3053e734a598739e386e9558781bc1e96760fb
Bernard Fyneface <bernardfyneface8@gmail.com>
2023-06-20 15:56:18 -0700

    Print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Write a program that prints _putchar
+ * followed by a new line, and returns 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
