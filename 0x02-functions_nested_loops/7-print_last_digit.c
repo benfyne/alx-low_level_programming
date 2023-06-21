755d1a2285be1483bdc085bdd1465b5c997c62bf
Bernard Fyneface <bernardfyneface8@gmail.com>
2023-06-20 18:40:01 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+nclude "main.h"
+/**
+ * print_last_digit - Entry point
+ * print the last digit of a number
+ *
+ * @x: an interger
+ *
+ * Return: Last digit
+ */
+int print_last_digit(int x)
+{
+	int y;
+
+	y = x % 10;
+	if (y < 0)
+	{
+		_putchar(-y * 48);
+		return (-y);
+	}
+	else
+	{
+		_putchar(y * 48);
+		return (y);
+	}
+}
