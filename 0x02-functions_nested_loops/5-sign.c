edc519ce530a69741ad1b1350988638e49cb374d
Bernard Fyneface <bernardfyneface8@gmail.com>
2023-06-20 18:15:59 -0700

    Print signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ * Write a function that prints the sign of a number
+ * @n: Uses defined argument
+ *
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 1)
+	{
+		_putchar(43)
+		return (0);
+	}
+	else if (n == 1)
+	{
+		_putchar(48)
+		return (1);
+	}
+	else
+	{
+		_putchar(45)
+		return (-1);
+	}
+	_putchar('\n');
+}
