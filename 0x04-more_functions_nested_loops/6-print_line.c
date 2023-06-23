70895de94ce84d60ab07416a1de800b7fbbbd336
Bernard Fyneface <bernardfyneface8@gmail.com>
2023-06-22 19:24:28 -0700

    Line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_line - uses the character _ to form a line.
+* 
+* @n: The number of times underscore is printed
+*
+* Return: void
+*/
+void print_line(int)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
