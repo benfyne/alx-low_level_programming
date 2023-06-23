ff4f5d2ccbcef848c69946c5e480d8ca929242e4
Bernard Fyneface <bernardfyneface8@gmail.com>
2023-06-22 18:49:43 -0700

    Most digit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - Write a function that prints the numbers, from 0 to 9
+ * Except 2 and 4
+ *
+ * Return: Void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if(i != 50 && 52)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
