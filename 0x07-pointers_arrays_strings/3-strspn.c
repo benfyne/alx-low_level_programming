7c51a5f3902c12d3d870abfad99d859c92560f09
Bernard Fyneface <bernardfyneface8@gmail.com>
2023-07-03 16:02:26 -0700

    length of prefix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,25 +9,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 0;
-	unsigned int j = 0;
+	unsigned int i;
+	unsigned int j;
 	unsigned int count = 0;
 
-	while (s[i])
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		j = 0;
-		while (accept[j])
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (accept[j] == s[i])
+			if (s[i] == accept[j])
 			{
 				count++;
 				break;
 			}
-			j++;
 		}
-		if (!accept[j])
-			return (count);
-		i++
+		if (accept[j] == '\0')
+			break;
 	}
 	return (count);
 }
