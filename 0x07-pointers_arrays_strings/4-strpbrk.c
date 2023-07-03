829a13d0c5232733f3366a54669144bab87fc1f3
Bernard Fyneface <bernardfyneface8@gmail.com>
2023-07-03 16:11:00 -0700

    searches a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strbrk - searches string for bytes
+ *
+ * @s: string
+ * @accept: string
+ *
+ * Return: s or null
+ */
+char *_strbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+	return (0);
+}
