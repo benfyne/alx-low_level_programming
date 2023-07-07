a3c16f89413ab2828036166793d59e62808a6aae
Bernard Fyneface <bernardfyneface8@gmail.com>
2023-07-07 08:07:47 -0700

    0

---
+++ 0x09-static_libraries/0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the program
+ *
+ * @argc: number of arguments
+ * passed to the program
+ * @argv: array of pointers to the argument
+ *
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
