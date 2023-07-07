c7684fb408f4847d550feeddb9b797ae7e143365
Bernard Fyneface <bernardfyneface8@gmail.com>
2023-07-07 14:13:22 -0700

    100

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the minimum number of coins
+ * to make change for an amount of money
+ *
+ * @argc: argument
+ * @argv: argument array
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int cents, coins, quaters, dimes, nickels, pennies;
+
+	if  (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	coins = 0;
+
+	quaters = cents / 25;
+	coins += quaters;
+	cents %= 25;
+
+	dimes = cents / 10;
+	coins += dimes;
+	cents %= 10;
+
+	nickels = cents / 5;
+	coins += nickels;
+	cent %= 5;
+
+	pennies = cents / 1;
+	coins += pennies;
+
+	printf("%d\n", coins);
+	return (0);
+
+}
