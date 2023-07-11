fc7351fd6c473dd1d5c70f2905f9f0c727c8c536
Bernard Fyneface <bernardfyneface8@gmail.com>
2023-07-10 18:15:10 -0700

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * strdup - retunrs pointer to new space
+ * in memory
+ *
+ * @str: char
+ *
+ * Return: null or str
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	int len, i;
+
+	if (str == NULL)
+		return (NULL);
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+	dup = malloc((len + 1) * sizeof(char));
+	if (dup == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		dup[i] = str[i];
+	dup[len] = '\0';
+	return (dup);
+}
