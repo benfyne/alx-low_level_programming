5b0f10777f90bf43eb36b00f27b219e09b94a2bc
Bernard Fyneface <bernardfyneface8@gmail.com>
2023-07-13 02:58:47 -0700

    0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -13,7 +13,27 @@ void *malloc_checked(unsigned int b)
 	p = malloc(b);
 	if (p == NULL)
 	{
-		fprintf(stderr, "Error: malloc failed\n");
+		_putchar('E');
+		_putchar('r');
+		_putchar('r');
+		_putchar('o');
+		_putchar('r');
+		_putchar(':');
+		_putchar(' ');
+		_putchar('m');
+		_putchar('a');
+		_putchar('l');
+		_putchar('l');
+		_putchar('o');
+		_putchar('c');
+		_putchar(' ');
+		_putchar('f');
+		_putchar('a');
+		_putchar('i');
+		_putchar('l');
+		_putchar('e');
+		_putchar('d');
+		_putchar('\n');
 		exit(98);
 	}
 	return (p);
