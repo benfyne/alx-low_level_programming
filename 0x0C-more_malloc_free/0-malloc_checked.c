aa57139cceae935a2cf03de8cf8c1c5a76c64c35
Bernard Fyneface <bernardfyneface8@gmail.com>
2023-07-13 02:40:42 -0700

    readme and task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * malloc_checked - allocates memory using malloc
+ *
+ * @b: the number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		fprint(stderr, "Error: malloc failed\n");
+		exit(98);
+	}
+	return (p);
+}
