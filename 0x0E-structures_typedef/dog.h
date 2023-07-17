92fc996dbf61ce968ef8c1bf6b6997b6aceb0768
Bernard Fyneface <bernardfyneface8@gmail.com>
2023-07-17 14:48:49 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,5 +19,10 @@ struct dog
 typedef struct dog dog_t;
 
 void print_dog(dog_t *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
