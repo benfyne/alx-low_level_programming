9d450f7d0373b44b08aed7edb9222a2e1cfeb1e1
Bernard Fyneface <bernardfyneface8@gmail.com>
2023-07-17 16:51:07 -0700

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,33 +1,56 @@
-#include "dog.h"
 #include <stdlib.h>
-#include <string.h>
+#include "dog.h"
+
 /**
- * new_dog - Write the function that creates a new dog
- *
- * @name: name
- * @age: age
- * @owner: owner
- *
- * Return: new dog
- */
+* new_dog - a function that creates a new dog
+* @name: a string 'name'
+* @age: a float 'age'
+* @owner: a string 'owner'
+* Return: returns a pointer to a struct
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
+	int i = 0, c = 0;
+	dog_t *newDog;
 
-	int name_len, owner_len;
+	newDog = malloc(sizeof(dog_t));
+	if (newDog == NULL)
+		return (NULL);
 
-	name_len = strlen(name);
-	owner_len = strlen(owner);
-	new_dog = malloc(sizeof(dog_t) + sizeof(char) * (name_len + 1)
-			+ sizeof(char) * (owner_len + 1));
-	if (new_dog == NULL)
+	newDog->age = age;
+
+	for (i = 0, c = 0; name[i] != '\0'; i++)
+		c++;
+	newDog->name = malloc(sizeof(char) * (c + 1));
+	if (newDog->name == NULL)
 	{
+		free(newDog);
 		return (NULL);
 	}
-	new_dog->name = (char *)(new_dog + 1);
-	strcpy(new_dog->name, name);
-	new_dog->owner = new_dog->name + name_len + 1;
-	strcpy(new_dog->owner, owner);
-	new_dog->age = age;
-	return (new_dog);
+
+	for (i = 0; 1 ; i++)
+	{
+		(*(newDog->name + i)) = name[i];
+		if (name[i] == '\0')
+			break;
+	}
+
+	for (i = 0, c = 0; owner[i] != '\0'; i++)
+		c++;
+	newDog->owner = malloc(sizeof(char) * (c + 1));
+	if (newDog->owner == NULL)
+	{
+		free(newDog);
+		free(newDog->name);
+		return (NULL);
+	}
+
+	for (i = 0; 1 ; i++)
+	{
+		(*(newDog->owner + i)) = owner[i];
+		if (owner[i] == '\0')
+			break;
+	}
+
+	return (newDog);
 }
