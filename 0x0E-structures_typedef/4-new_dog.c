908a68b2480a26c113cb7d4fec9820a4e549de43
Bernard Fyneface <bernardfyneface8@gmail.com>
2023-07-17 16:49:05 -0700

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,46 +14,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 
-	new_dog = malloc(sizeof(dog_t));
+	int name_len, owner_len;
 
+	name_len = strlen(name);
+	owner_len = strlen(owner);
+	new_dog = malloc(sizeof(dog_t) + sizeof(char) * (name_len + 1)
+			+ sizeof(char) * (owner_len + 1));
 	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
-	new_dog->name = copy_string(name);
-
-	if (new_dog->name == NULL)
-	{
-		free(new_dog);
-		return (NULL);
-	}
+	new_dog->name = (char *)(new_dog + 1);
+	strcpy(new_dog->name, name);
+	new_dog->owner = new_dog->name + name_len + 1;
+	strcpy(new_dog->owner, owner);
 	new_dog->age = age;
-	new_dog->owner = copy_string(owner);
-
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog->name);
-		free(new_dog);
-		return (NULL);
-	}
 	return (new_dog);
-}
-/**
- * copy_string - copies string
- *
- * @s: string
- *
- * Return: 0
- */
-char *copy_string(char *s)
-{
-	char *copy;
-
-	copy = malloc(sizeof(char) * (strlen(s) + 1));
-	if (copy == NULL)
-	{
-		return (NULL);
-	}
-	strcpy(copy, s);
-	return (copy);
 }
