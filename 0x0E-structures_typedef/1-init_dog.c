427282ab21090310f2da4408fd9c8d1367aa8d6c
Bernard Fyneface <bernardfyneface8@gmail.com>
2023-07-17 15:09:40 -0700

    1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,15 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
+	if (d == NULL)
+	{
+		d = malloc(sizeof(struct dog));
+		if (d == NULL)
+			return;
+	}
+	if (name != NULL)
+		d->name = name;
 	d->age = age;
-	d->owner = owner;
+	if (owner != NULL)
+		d->owner = owner;
 }
