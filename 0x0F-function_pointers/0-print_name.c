17582d8f2511efb74606df522e3a138d845da5f3
Bernard Fyneface <bernardfyneface8@gmail.com>
2023-07-19 12:21:18 -0700

    0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointer.h"
+#include <stdio.h>
 /**
  * print_name - prints name
  *
@@ -7,7 +8,34 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return
 	f(name);
+}
+/**
+ * print_in_caps - print caps
+ *
+ * @name: name
+ */
+void print_in_caps(char *name)
+{
+	while (*name)
+	{
+		if (*name >= 'a' && *name <= 'z')
+			_putchar(*name - 32);
+		else
+			_putchar(*name);
+		name++;
+	}
+	_putchar('\n');
+}
+/**
+ * main - name
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char *name = "John Doe";
+
+	print_name(name, print_in_caps);
+	return (0);
 }
