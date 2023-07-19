9560121041591ca744f71ca305eeb4a6c71d3fa1
Bernard Fyneface <bernardfyneface8@gmail.com>
2023-07-19 12:24:53 -0700

    00

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -10,32 +10,3 @@ void print_name(char *name, void (*f)(char *))
 {
 	f(name);
 }
-/**
- * print_in_caps - print caps
- *
- * @name: name
- */
-void print_in_caps(char *name)
-{
-	while (*name)
-	{
-		if (*name >= 'a' && *name <= 'z')
-			_putchar(*name - 32);
-		else
-			_putchar(*name);
-		name++;
-	}
-	_putchar('\n');
-}
-/**
- * main - name
- *
- * Return: 0
- */
-int main(void)
-{
-	char *name = "John Doe";
-
-	print_name(name, print_in_caps);
-	return (0);
-}
