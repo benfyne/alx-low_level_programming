926a14ba86741879e705534ac877e7ca94ccfda0
benfyne <133433515+benfyne@users.noreply.github.com>
2023-07-27 16:54:04 +0100

    Update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,18 +2,18 @@
 section .rodata
     msg: db "Hello, Holberton", 10 ; The message to print, followed by newline
     len: equ $ - msg ; The length of the message
-    format: db "%s", 0 ; The format string for printf
 
 ; Declare the main function
 section .text
     global main ; Make the main function visible to the linker
 
 main:
-    ; Call the printf function with the message as an argument
-    mov rdi, format ; First argument: the format string
+    ; Call the write system call with the message and its length as arguments
+    mov rdi, 1 ; First argument: the file descriptor (stdout)
     mov rsi, msg ; Second argument: the message to print
-    mov rax, 0 ; Indicate that there are no floating-point arguments
-    call printf ; Call the printf function
+    mov rdx, len ; Third argument: the length of the message
+    mov rax, 1 ; The system call number for write
+    syscall ; Invoke the system call
 
     ; Exit the program with 0 as the return code
     mov rdi, 0 ; First argument: the return code
