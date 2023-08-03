d159a934ae1734157a599bf05cfe675f906f0bc7
Bernard Fyneface <bernardfyneface8@gmail.com>
2023-08-03 10:54:18 -0700

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,22 +5,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	print_binary_helper(n);
-}
-/**
- * print_binary_helper - prints the binary
- * representation of a number in reverse order
- * @n: the number to print
- */
-void print_binary_helper(unsigned long int n)
-{
-	if (n == 0)
-		return;
-	print_binary_helper(n >> 1);
+	if (n > 0)
+		print_binary(n >> 1);
 	_putchar((n & 1) + '0');
 }
